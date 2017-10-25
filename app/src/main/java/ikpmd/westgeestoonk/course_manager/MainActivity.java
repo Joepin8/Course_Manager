4b3fab319d97dbbe7dc9d0337ae6e8fe01b304c2
Tommy Westgeest <wtommywestgeest@gmail.com>
2017-10-25 22:09:26 +0200

    Account toevoegen

--- app/src/main/java/ikpmd/westgeestoonk/course_manager/MainActivity.java
+++ app/src/main/java/ikpmd/westgeestoonk/course_manager/MainActivity.java
@@ -1,29 +1,76 @@
 package ikpmd.westgeestoonk.course_manager;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth fAuth;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginBtn;
+    private Button newAccountBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference mChildref = mRootRef.child("jaar1").child("IARCH");
+        DatabaseReference mChildRef = mRootRef.child("test");
+        fAuth = FirebaseAuth.getInstance();
 
-        final TextView tv = (TextView) findViewById(R.id.tv);
+        emailEditText = (EditText) findViewById(R.id.emailET);
+        passwordEditText = (EditText) findViewById(R.id.passwordET);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        newAccountBtn = (Button) findViewById(R.id.newAccountBtn);
 
-        Log.d("DEBUG", mChildref.toString() + ".json");
+        loginBtn.setText("Inloggen");
+        newAccountBtn.setText("Nieuw account");
+
+        fAuth.signOut();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("DEBUG", "Login btn fired");
+                if(emailEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Inloggen mislukt", Toast.LENGTH_SHORT).show();
+                } else {
+                    fAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Log.d("DEBUG", "ingelogd");
+                                Toast.makeText(getApplicationContext(), "Ingelogd", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Inloggen mislukt", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        newAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), NieuwAccount.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
