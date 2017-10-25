d8fb9e5b71ca61cbfb39254830ebd8d040673212
Tommy Westgeest <wtommywestgeest@gmail.com>
2017-10-25 18:22:14 +0200

    setup firebase

--- app/src/main/java/ikpmd/westgeestoonk/course_manager/MainActivity.java
+++ app/src/main/java/ikpmd/westgeestoonk/course_manager/MainActivity.java
@@ -2,6 +2,13 @@ package ikpmd.westgeestoonk.course_manager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mChildref = mRootRef.child("test");
+
+        final TextView tv = (TextView) findViewById(R.id.tv);
+
+        mChildref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tv.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
