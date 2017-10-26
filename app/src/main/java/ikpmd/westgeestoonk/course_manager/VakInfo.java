31314afefb219eb9a64cc717613670de3c9c0504
Joepin8 <joep_oonk@hotmail.com>
2017-10-26 21:37:10 +0200

    Vakinfo view

---
+++ app/src/main/java/ikpmd/westgeestoonk/course_manager/VakInfo.java
@@ -0,0 +1,15 @@
+package ikpmd.westgeestoonk.course_manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VakInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vak_info);
+        setTitle("Vak Informatie");
+
+    }
+}
